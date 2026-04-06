8f4458c98616ca3eb352e91571c6b3dfb4cd21af
Rafaelfraga99 <rafaelfraga99@users.noreply.github.com>
2026-04-05 23:46:27 -0400

    Add Products to navigation

---
+++ src/MyNav.tsx
@@ -0,0 +1,37 @@
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function MyNav() {
+  return (
+    <BrowserRouter basename="/web-development-2">
+      <SetNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function SetNav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default MyNav;
---
+++ src/Products.tsx
@@ -0,0 +1,35 @@
+function Products() {
+  return (
+    <table>
+      <head>
+        <tr>
+          <th>#</th>
+          <th>Product Name</th>
+          <th>Cost</th>
+        </tr>
+      </head>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Apple</td>
+          <td>Fruit</td>
+          <td>$1.00</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Pineapple</td>
+          <td>Fruit</td>
+          <td>$3.00</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Cucumber</td>
+          <td>Vegetable</td>
+          <td>$4.00</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Products;
